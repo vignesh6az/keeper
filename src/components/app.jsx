b61cd52d2141a30efe786ed8890893c2763c7938
Vignesh <vignesh@lingapos.com>
2022-08-19 01:02:22 +0530

    initial commit

---
+++ src/components/app.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./heading";
+import Footer from "./footer";
+import Note from "./notes";
+
+function App(){
+    return(
+    <div>
+        <Header />
+        <Footer />
+        <Note />
+    </div>
+       
+    );
+
+}
+
+export default App;
---
+++ src/components/notes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return (
+    <div className="note">
+        <h1>This is Title</h1>
+        <p>This is Note</p>
+    </div>
+    );
+}
+
+export default Note;
